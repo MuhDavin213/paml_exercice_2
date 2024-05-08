b5179e258f4145b4c33d4c41c379514d98687cf5
Muhammad Davin Widyatmaka <davin.new58@gmail.com>
2024-05-08 15:11:30 +0700

    make API and Test API to Detail Screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +12,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List _listdata = [];
+
+  Future _getdata() async {
+    try {
+      final Response = await http
+          .get(Uri.parse('http://192.168.1.11/api/restoapi/read.php'));
+      if (Response.statusCode == 200) {
+        final data = jsonDecode(Response.body);
+        setState(() {
+          _listdata = data;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    _getdata();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Kuliner Tour"),
+      ),
+      body: ListView.builder(
+        itemCount: _listdata.length,
+        itemBuilder: ((context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(_listdata[index]['nama']),
+              subtitle: Text(_listdata[index]['rate_resto']),
+            ),
+          );
+        }),
       ),
     );
   }
