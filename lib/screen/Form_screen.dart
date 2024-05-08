4e5accf556eca2d42fa4ac1f597914472be24cb6
Muhammad Davin Widyatmaka <davin.new58@gmail.com>
2024-05-08 16:27:15 +0700

    add a controller for an editable text field and add button simpan di formsreen

--- lib/screen/Form_screen.dart
+++ lib/screen/Form_screen.dart
@@ -8,6 +8,11 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  TextEditingController rate_resto = TextEditingController();
+  TextEditingController nama = TextEditingController();
+  TextEditingController alamat = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,45 +21,59 @@ class _FormScreenState extends State<FormScreen> {
         backgroundColor: Color.fromARGB(146, 7, 212, 205),
         elevation: 15,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Form(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Rating Restaurant",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: rate_resto,
+                  decoration: InputDecoration(
+                    hintText: "Rating Restaurant",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Nama Restaurant",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: nama,
+                  decoration: InputDecoration(
+                    hintText: "Nama Restaurant",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Alamat Restaurant",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: alamat,
+                  decoration: InputDecoration(
+                    hintText: "Alamat Restaurant",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 32, 231, 195)),
+                    onPressed: () {},
+                    child: Text("Simpan"))
+              ],
+            ),
           ),
         ),
       ),
